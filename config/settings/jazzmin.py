be1b6ea89362c01567f492b2d4d5b45c0bc4e860
mswietlik <matiks@gmail.com>
2023-10-25 15:37:20 +0200

    quick fix

--- config/settings/jazzmin.py
+++ config/settings/jazzmin.py
@@ -77,7 +77,7 @@ JAZZMIN_SETTINGS = {
     "navigation_expanded": True,
 
     # Hide these apps when generating side menu e.g (auth)
-    "hide_apps": [],
+    "hide_apps": ['socialaccount', 'authtoken'],
 
     # Hide these models when generating side menu (e.g auth.user)
     "hide_models": [],
