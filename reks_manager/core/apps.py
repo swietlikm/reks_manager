710e67f3ec1b007d8dca3c250046a8d2b11927d2
Mateusz Świetlik <matiks@gmail.com>
2024-02-04 17:18:47 +0100

    fix tests and formatting

--- reks_manager/core/apps.py
+++ reks_manager/core/apps.py
@@ -5,9 +5,3 @@ from django.utils.translation import gettext_lazy as _
 class UsersConfig(AppConfig):
     name = "reks_manager.core"
     verbose_name = _("Core")
-
-    def ready(self):
-        try:
-            import reks_manager.users.signals  # noqa: F401
-        except ImportError:
-            pass
